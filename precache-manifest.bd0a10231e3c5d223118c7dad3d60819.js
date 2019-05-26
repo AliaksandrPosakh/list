d8662d84654687c1bc8cad65f4ed5141858c1738
AliaksandrPosakh <arsenev_alesha@mail.ru>
2019-05-26 17:22:02 +0300

    Updates

---
+++ precache-manifest.bd0a10231e3c5d223118c7dad3d60819.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "893017e7fd6b6b846040",
+    "url": "/list/static/js/runtime~main.7fc17663.js"
+  },
+  {
+    "revision": "ce51bd7bd531945d7813",
+    "url": "/list/static/js/main.a8af06f7.chunk.js"
+  },
+  {
+    "revision": "97438886f9c23de7cb0e",
+    "url": "/list/static/js/2.d2298ced.chunk.js"
+  },
+  {
+    "revision": "ce51bd7bd531945d7813",
+    "url": "/list/static/css/main.b05ebc1d.chunk.css"
+  },
+  {
+    "revision": "dfdbff9746fc3b49ad68c4a93f8b39d8",
+    "url": "/list/index.html"
+  }
+];
